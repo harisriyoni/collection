ff1706e9f3a152e940db2e391352434d7aeea77d
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-08 10:37:58 +0700

    test push collectiona n map

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -2,20 +2,38 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CollectionTest extends TestCase
 {
-   public function testCreateCollection(){
-     $collection = collect([1,2,3]);
-     $this->assertEqualsCanonicalizing([1,2,3], $collection->all());
-   }
-   public function testForEach(){
-    $collection = collect([1,2,3,4,5,6,7,8,9]);
-    foreach ($collection as $key => $value){
-        $this->assertEquals($key + 1,  $value);
+    public function testCreateCollection()
+    {
+        $collection = collect([1, 2, 3]);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
     }
-   }
+    public function testForEach()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        foreach ($collection as $key => $value) {
+            $this->assertEquals($key + 1, $value);
+        }
+    }
+    public function testCrud()
+    {
+        $collection = collect([]);
+        $collection->push(1, 2, 3);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+
+        $result = $collection->pop();
+        $this->assertEquals(3, $result);
+        $this->assertEqualsCanonicalizing([1, 2], $collection->all());
+    }
+    public function testMap()
+    {
+        $collection = collect([1, 2, 3]);
+        $result = $collection->map(function ($item) {
+            return $item * 2;
+        });
+        $this->assertEqualsCanonicalizing([2, 4, 6], $result->all());
+    }
 }
