b4f92b86128dba8c3059e5d76a2381d72c0c53c9
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-08 10:10:17 +0700

    collection test dna test for each di collection

---
+++ tests/Feature/CollectionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CollectionTest extends TestCase
+{
+   public function testCreateCollection(){
+     $collection = collect([1,2,3]);
+     $this->assertEqualsCanonicalizing([1,2,3], $collection->all());
+   }
+   public function testForEach(){
+    $collection = collect([1,2,3,4,5,6,7,8,9]);
+    foreach ($collection as $key => $value){
+        $this->assertEquals($key + 1,  $value);
+    }
+   }
+}
