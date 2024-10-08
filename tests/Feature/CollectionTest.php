6fccda274bb93231e657b8b118b31686cb1d0613
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-08 11:07:40 +0700

    map spread map $ implementasi class person in map

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Data\Person;
+use function PHPUnit\Framework\assertEquals;
 use Tests\TestCase;
 
 class CollectionTest extends TestCase
@@ -35,5 +37,53 @@ class CollectionTest extends TestCase
             return $item * 2;
         });
         $this->assertEqualsCanonicalizing([2, 4, 6], $result->all());
+    }
+    public function testMapInto()
+    {
+        $collection = collect(["Haris"]);
+        $result = $collection->mapInto(Person::class);
+        $this->assertEquals([new Person("Haris")], $result->all());
+    }
+    public function testMapSpread()
+    {
+        $collection = collect([["Haris", "Riyoni"], ["Dono", "Kasino"]]);
+        $result = $collection->mapSpread(function ($firstname, $lastname) {
+            $fullname = $firstname . ' ' . $lastname;
+            return new Person($fullname);
+        });
+        $this->assertEquals([
+            new Person("Haris Riyoni"),
+            new Person("Dono Kasino"),
+        ], $result->all());
+    }
+    public function testMapToGroups()
+    {
+        $collection = collect([
+            [
+                "Name" => "Haris",
+                "Department" => "IT",
+            ],
+            [
+                "Name" => "Dono",
+                "Department" => "IT",
+            ],
+            [
+                "Name" => "Kasino",
+                "Department" => "HR",
+            ],
+            [
+                "Name" => "Dono",
+                "Department" => "CEO",
+            ],
+        ]);
+
+        $result = $collection->mapToGroups(function ($item) {
+            return [$item["Department"] => $item["Name"]];
+        });
+        assertEquals([
+            "IT" => collect(["Haris", "Dono"]),
+            "HR" => collect(["Kasino"]),
+            "CEO" => collect(["Dono"]),
+        ], $result->all());
     }
 }
