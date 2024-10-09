f317bf9475f7b737996c0e476d111c04eb59844e
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-09 11:49:29 +0700

    filter test

--- tests/Feature/FilterTest.php
+++ tests/Feature/FilterTest.php
@@ -2,26 +2,34 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
-
 use function PHPUnit\Framework\assertEquals;
+
+use LDAP\Result;
+use Tests\TestCase;
 
 class FilterTest extends TestCase
 {
-  public function testFilter(){
-    $collection = collect([
-        "haris" => "100",
-        "dono" => "20",
-        "dini" => "110",
-    ]);
-    $result = $collection->filter(function($value, $key){
-        return $value >=90;
-    });
-    assertEquals([
-        "haris"=>"100",
-        "dini"=>"110",
-    ], $result->all());
-  }
+    public function testFilter()
+    {
+        $collection = collect([
+            "haris" => "100",
+            "dono" => "20",
+            "dini" => "110",
+        ]);
+        $result = $collection->filter(function ($value, $key) {
+            return $value >= 90;
+        });
+        assertEquals([
+            "haris" => "100",
+            "dini" => "110",
+        ], $result->all());
+    }
+    public function testIndex()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        $result = $collection->filter(function ($value, $key) {
+            return $value %2==0;
+        });
+        $this->assertEqualsCanonicalizing([2,4,6,8,10], $result->all());
+    }
 }
