a91bd206c49e9657f2cadab000ad13c98f59e351
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-09 14:18:42 +0700

    udpate chunk

--- tests/Feature/ChunkTest.php
+++ tests/Feature/ChunkTest.php
@@ -2,19 +2,17 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ChunkTest extends TestCase
 {
     public function testChunk()
     {
-        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         $result = $collection->chunk(3);
-        $this->assertEqualsCanonicalizing([1,2,3], $result->all()[0]->all());
-        $this->assertEqualsCanonicalizing([4,5,6], $result->all()[1]->all());
-        $this->assertEqualsCanonicalizing([7,8,9], $result->all()[2]->all());
-
+        $this->assertEqualsCanonicalizing([1, 2, 3], $result->all()[0]->all());
+        $this->assertEqualsCanonicalizing([4, 5, 6], $result->all()[1]->all());
+        $this->assertEqualsCanonicalizing([7, 8, 9], $result->all()[2]->all());
+        $this->assertEqualsCanonicalizing([10], $result->all()[3]->all());
     }
 }
